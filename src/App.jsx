9abce49b98762ee8c0933408e283a3d38b530219
jerrymusaga <musagatechnology@gmail.com>
2023-08-24 18:35:01 +0100

    alert and loading ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
+import Alert from "./components/Alert"
 import ArtWorks from "./components/Artworks"
 import Footer from "./components/Footer"
 import Hero from "./components/Hero"
+import Loading from "./components/Loading"
 import MintNFT from "./components/MintNFT"
 import Navigation from "./components/Navigation"
 import NFTDetails from "./components/NFTDetails"
@@ -20,6 +22,8 @@ const App = () => {
     <NFTDetails />
     <UpdateNFT />
     <ReactionsModal />
+    <Loading />
+    <Alert />
 
   </div>
   )
--- src/store/index.js
+++ src/store/index.js
@@ -5,6 +5,22 @@ const {setGlobalState, getGlobalState, useGlobalState} = createGlobalState({
     nftDetailModal: 'scale-0',
     updateNFTModal: 'scale-0',
     reactionModal: 'scale-0',
+    loading: {show: false, msg: ''},
+    alert: {show:false, msg:'', color:''},
 });
 
-export {setGlobalState, getGlobalState, useGlobalState}
+const setAlert = (msg,color='green') => {
+    setGlobalState('loading', {show:false,msg:''})
+    setGlobalState('alert', {show:true, msg, color})
+    setTimeout(() => {
+        setGlobalState('alert',{show:false, msg, color})
+    }, 6000)
+}
+
+const setLoadingMsg = (msg) => {
+    const loading = getGlobalState('loading')
+    setGlobalState('loading', {...loading, msg})
+}
+
+
+export {setGlobalState, getGlobalState, useGlobalState, setAlert, setLoadingMsg}
